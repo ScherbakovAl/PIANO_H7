3ce180638ec472ffcbbf33365818681bb4c7bc36
Scherbakov.al <scherbakov.al@gmail.com>
2025-05-29 11:40:09 +0300

    ..

--- Piano_H7/usb_descriptors.c
+++ Piano_H7/usb_descriptors.c
@@ -119,10 +119,7 @@ enum {
 
 // array of pointer to string descriptors
 char const* string_desc_arr[] =
-{
-  (const char[]) {
- 0x09, 0x04
-}, // 0: is supported language is English (0x0409)
+{ (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
 "TinyUSB",                     // 1: Manufacturer
 "TinyUSB Device",              // 2: Product
 NULL,                          // 3: Serials will use unique ID if possible
