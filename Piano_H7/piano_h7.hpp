d111a098df5e59d0e9a3777a2b21f90fb52fcf72
Scherbakov.al <scherbakov.al@gmail.com>
2025-09-08 19:33:46 +0300

    cubeMX update for U1->U2, pA8->pB7

--- Piano_H7/piano_h7.hpp
+++ Piano_H7/piano_h7.hpp
@@ -22,9 +22,9 @@ extern "C" {
 	using cuint = const uint;
 
 	const int allChipCount = 26;
-	const int32_t start_cursor = 34; // 34 -7й датчик у 4й платы
+	const int32_t start_cursor = 7; // 34 -7й датчик у 4й платы
 	const int start_sensor = 1; // включительно
-	const int end_sensor = 6; // включительно
+	const int end_sensor = 3; // включительно
 	//const int chipAdress = 1; // для NoteOff что-то придумать надо здесь!
 	// ***** 390(263)-14000(22723)us пролёт молоточка
 	
