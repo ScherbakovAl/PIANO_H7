1859d71b8fc9691bf40b4e7347b6555fb842f0e7
Scherbakov.al <scherbakov.al@gmail.com>
2026-01-02 21:10:53 +0300

    ..

--- Piano_H7/piano_h7.cpp
+++ Piano_H7/piano_h7.cpp
@@ -190,18 +190,7 @@ void h7() {
 	debugg_clear();
 	debug_counter = 1;
 	int test_int_timer2_old = test_int_timer2;
-
-	while (1) {
-		UART4_SendAddress(1);
-		pause(4);
-		UART4_Send_Settings(command::cal, 1, 0, 0);
-		UART4_Receive_Settings();
-		// UART4_Receive_Settings();
-		pause(2000);
-	}
-
-
-
+	
 	while (1) {
 		tud_task();
 		lv_timer_handler();
