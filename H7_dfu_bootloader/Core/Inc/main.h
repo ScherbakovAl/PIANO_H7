9fdbfb410ded26df8aeb3924b36af567258dad96
Scherbakov.al <scherbakov.al@gmail.com>
2026-01-27 12:27:38 +0300

    ))) теперь работает кастомный bootloader !!!!

--- H7_dfu_bootloader/Core/Inc/main.h
+++ H7_dfu_bootloader/Core/Inc/main.h
@@ -42,7 +42,7 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "usbd_dfu_if.h"
+
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
